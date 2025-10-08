c595b55f6825104b18d080f8d85114bf35938b81
HemanthSeelam <164900454+HemanthSeelam@users.noreply.github.com>
2025-10-08 21:20:28 +0530

    Add files via upload

---
+++ number_guessing.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int number = rand() % 100 + 1;
+    int guess, attempts = 0;
+    cout << "Guess the number between 1 and 100\n";
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+        if (guess > number)
+            cout << "Too high!\n";
+        else if (guess < number)
+            cout << "Too low!\n";
+        else
+            cout << "Correct! You guessed it in " << attempts << " attempts.\n";
+    } while (guess != number);
+    return 0;
+}
